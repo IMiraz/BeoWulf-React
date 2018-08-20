ec2581fbd87919c8644ba5428671a3e65517c9e4
wasifmiraz <wasifmiraz@mail.com>
2018-08-20 19:15:50 +0600

    router up

---
+++ src/components/routes/GuestRoute.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Route, Redirect} from 'react-router-dom';
+import propTypes from 'prop-types'
+import {connect} from 'react-redux'
+
+const UserRoute = ({isAuthenticated, component: Component, ...rest }) => {
+  return (
+    <Route {...rest} render = {props =>
+     !isAuthenticated? <Component {...props}/>: <Redirect to="/dashborad"/>} />
+  )
+}
+
+UserRoute.propTypes ={
+component:propTypes.func.isRequired,
+isAuthenticated:propTypes.bool.isRequired
+}
+
+function mapStateToProps(state) {
+    return{
+        isAuthenticated:!!state.user.token
+    }
+
+}
+
+
+
+export default connect(mapStateToProps) (UserRoute);
---
+++ src/components/routes/UserRoute.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Route, Redirect} from 'react-router-dom';
+import propTypes from 'prop-types'
+import {connect} from 'react-redux'
+
+const UserRoute = ({isAuthenticated, component: Component, ...rest }) => {
+  return (
+    <Route {...rest} render = {props => isAuthenticated? <Component {...props}/>: <Redirect to="/"/>} />
+  )
+}
+
+UserRoute.propTypes ={
+component:propTypes.func.isRequired,
+isAuthenticated:propTypes.bool.isRequired
+}
+
+function mapStateToProps(state) {
+    return{
+        isAuthenticated:!!state.user.token
+    }
+
+}
+
+
+
+export default connect(mapStateToProps) (UserRoute);
