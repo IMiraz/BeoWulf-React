6a5c4e96efde28e965f9a21e466284e8af2e3d21
wasifmiraz <wasifmiraz@mail.com>
2018-08-27 17:03:56 +0600

    new books page up

--- src/components/forms/SearchBookForm.js
+++ src/components/forms/SearchBookForm.js
@@ -1,23 +1,15 @@
 import React, { Component } from 'react'
 import { Form, Dropdown } from 'semantic-ui-react';
+import propTypes from 'prop-types'
+
+import axios from 'axios'
 
 export default class SearchBookForm extends Component
 {
    state = {
        query:'',
        loading:false,
-       options:[
-{
-key:1,
-value:1,
-text:"first books"
-},
-{
-    key:2,
-    value:2,
-    text:"react  books"
-    },
-],
+       options:[],
 books:{}
 }
 onSearchChange =(e,data) => {
@@ -26,11 +18,34 @@ this.setState({
 query:data
 });
 this.timer =setTimeout(this.fetchOption, 1000);
+};
+onChange = (e, data) => {
+this.setState({ query:data.value});
+this.props.onBookSelect(this.state.books[data.value])
 }
+
+
 fetchOption = () =>
 {
     if(!this.state.query) return;
- this.setState({loading:true})
+ this.setState({loading:true});
+axios.get(`/api/books/search?q=${this.state.query}`)
+.then(res =>res.data.books)
+.then(books => {
+  const options =[];
+  const booksHash={};
+books.forEach(book => {
+ booksHash[book.goodreadId]= book;
+options.push({
+  key:book.goodreadId,
+  value:book.goodreadId,
+text:book.title
+
+});
+
+});
+this.setState({loading:false, options, books:booksHash})
+})
 }
 
 
@@ -46,6 +61,7 @@ value={this.state.query}
 onSearchChange={this.onSearchChange}
 options={this.state.options}
 loading={this.state.loading}
+onChange={this.onChange}
 >
 
 </Dropdown>
@@ -54,4 +70,8 @@ loading={this.state.loading}
       </div>
     )
   }
+}
+
+SearchBookForm.propTypes= {
+  onBookSelect:propTypes.func.isRequired
 }
--- src/components/pages/NewBooksPage.js
+++ src/components/pages/NewBooksPage.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react'
 import { Segment } from 'semantic-ui-react';
 import SearchBookForm from '../forms/SearchBookForm'
+import BookForm from '../forms/BookForm'
 
  class NewBooksPage extends Component {
      state = {
   book:null
-     }
+     };
+
+     onBookSelect = book =>this.setState({ book});
+
+  addBook = () => console.log('h1');
   render() {
     return (
    <Segment>
 <h1>Add New Book Your Collection</h1>
-<SearchBookForm/>
+<SearchBookForm onBookSelect={this.onBookSelect}/>
+{this.state.book && <BookForm submit ={this.addBook} book={this.state.book}/>}
 
    </Segment>
     )
